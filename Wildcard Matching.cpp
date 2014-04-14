6f320b36df8982604dd2a3dea0a8c7738192fcbe
pysoftmac <pysoftmac@gmail.com>
2014-04-14 12:04:59 +0800

    Time limit Exceeded

---
+++ Wildcard Matching.cpp
@@ -0,0 +1,38 @@
+#include "util.h"
+
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        if(*p == '\0')
+			return *s == '\0';
+
+		if(*p == '*')
+		{
+			++p;
+			while(true)
+			{
+				if(isMatch(s,p))
+					return true;
+				if(*s != '\0')
+					++s;
+				else
+					break;
+			}
+		}
+		else
+		{
+			if(*s != '\0' && (*p == '?' || *p == *s))
+				return isMatch(s + 1, p + 1);
+		}
+
+		return false;
+    }
+
+	void test()
+	{
+		if(isMatch("abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb", "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb"))
+			cout << "True" << endl;
+		else
+			cout << "False" << endl;
+	}
+};
