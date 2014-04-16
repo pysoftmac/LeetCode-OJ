d4bb4da349cdbfa8177b3c0e216a5245aeed89dc
pysoftmac <pysoftmac@gmail.com>
2014-04-16 23:36:33 +0800

    use long long

---
+++ Pow(x, n).cpp
@@ -0,0 +1,22 @@
+#include "util.h"
+
+class Solution {
+public:
+    double pow(double x, /*int*/ long long n) 
+	{
+		if(n < 0)
+			return 1 / pow(x, -n);
+		double ans = 1;
+		for(; n > 0; x *= x, n >>= 1)
+		{
+			if(n & 1)
+				ans *= x;
+		}
+		return ans;
+    }
+
+	void test()
+	{
+		cout << pow(2,2) << endl;
+	}
+};
