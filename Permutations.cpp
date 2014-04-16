fa057a1e74ea2e67f3e1a4c4a39d2135c1b66e4c
pysoftmac <pysoftmac@gmail.com>
2014-04-16 11:19:36 +0800

    dfs

---
+++ Permutations.cpp
@@ -0,0 +1,48 @@
+#include "util.h"
+
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) 
+	{
+		vector<int> sign(num.size(), 0);
+		vector<int> path;
+		vector<vector<int> > res;
+		dfs(num, sign, path, res);
+		return res;
+    }
+
+	void dfs(vector<int> &num, vector<int>& sign, vector<int> &path, vector<vector<int> > &res)
+	{
+		if(path.size() == num.size())
+		{
+			res.push_back(path);
+			return;
+		}
+		for(int i = 0; i < (int)num.size(); i++)
+		{
+			if(sign[i] == 0)
+			{
+				sign[i] = 1;
+				path.push_back(num[i]);
+				dfs(num, sign, path, res);
+				sign[i] = 0;
+				path.pop_back();
+			}
+		}
+	}
+
+	void test()
+	{
+		int A[] = {1, 2, 3};
+		vector<int> num(A, A + 3);
+		vector<vector<int> > res = permute(num);
+		for(int i = 0; i < (int)res.size(); i++)
+		{
+			for(int j = 0; j < (int)res[i].size(); j++)
+			{
+				cout << res[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+};
