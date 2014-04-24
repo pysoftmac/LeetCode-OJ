f42bb5ee03b938186814eb977c4e73f4028346c1
pysoftmac <pysoftmac@gmail.com>
2014-04-24 10:04:39 +0800

    ok

---
+++ Length of Last Word.cpp
@@ -0,0 +1,27 @@
+#include "util.h"
+
+class Solution {
+public:
+    int lengthOfLastWord(const char *s) {
+        int len = strlen(s);
+		int pos = -1;
+		while(len > 0)
+		{
+			if(s[len - 1] != ' ')
+				break;
+			len--;
+		}
+		for(int i = len - 1; i >= 0; i--)
+		{
+			if(s[i] == ' ')
+			{
+				pos = i;
+				break;
+			}
+		}
+		if(pos == -1)
+			return len;
+		else
+			return len - 1 - pos;
+    }
+};
