2e6ae52b6227f8933b609db59f27da967ab586c1
pysoftmac <pysoftmac@gmail.com>
2014-04-27 17:52:38 +0800

    ok

---
+++ Plus One.cpp
@@ -0,0 +1,22 @@
+#include "util.h"
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        vector<int> res;
+
+		int nSize = digits.size();
+		int carry = 1;
+		for(int i = nSize - 1; i >= 0; i--)
+		{
+			carry += digits[i];
+			digits[i] = carry % 10;
+			carry /= 10;
+		}
+
+		res.push_back(carry);
+		for(int i = 0; i < nSize - 1; i++)
+			res.push_back(digits[i]);
+		return res;
+    }
+};
