2274d7e5a6ebf6ee4e01254238f34fa2c1efdefb
pysoftmac <pysoftmac@gmail.com>
2014-05-06 22:13:48 +0800

    swap

---
+++ Sort Colors.cpp
@@ -0,0 +1,32 @@
+#include "util.h"
+
+class Solution {
+public:
+    void sortColors(int A[], int n) {
+        int i = 0; // 0 pointer
+		int j = n - 1; // 1 pointer
+		int k = n - 1; // 2 pointer
+
+		while(i <= j)
+		{
+			if(A[i] == 2)
+			{
+				int t = A[i];
+				A[i] = A[k];
+				A[k] = t;
+				k--;
+				if(k < j)
+					j = k;
+			}
+			else if(A[i] == 1)
+			{
+				int t = A[i];
+				A[i] = A[j];
+				A[j] = t;
+				j--;
+			}
+			else
+				i++;
+		}
+    }
+};
