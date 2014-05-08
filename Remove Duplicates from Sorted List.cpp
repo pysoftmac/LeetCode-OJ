82cf52b141fed874980f82cd10136375a223e24a
pysoftmac <pysoftmac@gmail.com>
2014-05-08 22:47:02 +0800

    ok

---
+++ Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,36 @@
+#include "util.h"
+
+ // Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode* p = head, *q;
+		if(p != NULL)
+		{
+			q = p->next;
+		}
+
+		while(q != NULL)
+		{
+			if(p->val == q->val)
+			{
+				p->next = q->next;
+				delete q;
+				q = p->next;
+				continue;
+			}
+			else
+			{
+				p = p->next;
+				q = q->next;
+			}
+		}
+		return head;
+    }
+};
