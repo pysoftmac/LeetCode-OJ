a01968f0072554a9a1dbe29652a60ed232c34b67
pysoftmac <pysoftmac@gmail.com>
2014-05-08 22:33:58 +0800

    ok

---
+++ Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,35 @@
+#include "util.h"
+
+class Solution {
+public:
+    bool search(int A[], int n, int target) {
+        int l = 0, r = n - 1, mid;
+
+		while(l <= r)
+		{
+			mid = (l + r) / 2;
+			if(A[mid] == target)
+				return true;
+			else if(A[l] < A[mid])
+			{
+				if(A[l] <= target && target < A[mid])
+					r = mid - 1;
+				else
+					l = mid + 1;
+			}
+			else if(A[mid] < A[r])
+			{
+				if(A[mid] < target && target <= A[r])
+					l = mid + 1;
+				else
+					r = mid - 1;
+			}
+			else if(A[l] == A[mid])
+				l++;
+			else if(A[r] == A[mid])
+				r--;
+		}
+
+		return false;
+    }
+};
