cc88a6167b46b3eafc7a55848c5457263e7dfee1
pysoftmac <pysoftmac@gmail.com>
2014-05-08 10:51:32 +0800

    dfs

---
+++ Combinations.cpp
@@ -0,0 +1,31 @@
+#include "util.h"
+
+class Solution {
+public:
+    vector<vector<int> > combine(int n, int k) {
+        vector<int> path;
+		vector<vector<int> > res;
+		dfs(path, res, 1, n, k);
+		return res;
+    }
+
+	void dfs(vector<int> &path, vector<vector<int> > &res, int idx, int &n, int &k)
+	{
+		if((int)path.size() == k)
+		{
+			res.push_back(path);
+		}
+		else
+		{
+			if(path.size() < k && idx <= n)
+			{
+				for(int i = idx; i <= n; i++)
+				{
+					path.push_back(i);
+					dfs(path, res, i + 1, n, k);
+					path.pop_back();
+				}
+			}
+		}
+	}
+};
---
+++ Subsets.cpp
@@ -0,0 +1,28 @@
+#include "util.h"
+
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+		sort(S.begin(), S.end());
+		reverse(S.begin(), S.end());
+        vector<int> path;
+		vector<vector<int> > res;
+		dfs(path, res, S, 0);
+		return res;
+    }
+
+	void dfs(vector<int> &path, vector<vector<int> > &res, vector<int> &S, int idx)
+	{
+		res.push_back(path);
+
+		if(path.size() < S.size() && idx < (int)S.size())
+		{
+			for(int i = idx; i < (int)S.size(); i++)
+			{
+				path.push_back(S[i]);
+				dfs(path, res, S, i + 1);
+				path.pop_back();
+			}
+		}
+	}
+};
