87cf1abeb843d272eed1228afc430e40b8672c83
pysoftmac <pysoftmac@gmail.com>
2014-05-09 23:53:25 +0800

    ok

---
+++ Largest Rectangle in Histogram.cpp
@@ -0,0 +1,58 @@
+#include "util.h"
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int> &height) {
+		if((int)height.size() == 0)
+			return 0;
+
+		int maxArea = 0;
+
+		stack<int> stackIndex;
+		stack<int> stackHeight;
+
+		for(int i = 0; i < (int)height.size(); i++)
+		{
+			if(stackIndex.empty() || height[i] > stackHeight.top())
+			{
+				stackIndex.push(i);
+				stackHeight.push(height[i]);
+			}
+			else
+			{
+				if(height[i] < stackHeight.top())
+				{
+					int lastIndex = 0;
+					while(!stackIndex.empty() && height[i] <= stackHeight.top())
+					{
+						int tempArea = stackHeight.top() * (i - stackIndex.top());
+						if(tempArea > maxArea)
+							maxArea = tempArea;
+						lastIndex = stackIndex.top();
+						stackIndex.pop();
+						stackHeight.pop();
+					}
+					stackIndex.push(lastIndex);
+					stackHeight.push(height[i]);
+				}
+			}
+		}
+
+		while(!stackIndex.empty())
+		{
+			int tempArea = stackHeight.top() * ((int)height.size() - stackIndex.top());
+			if(tempArea > maxArea)
+				maxArea = tempArea;
+			stackIndex.pop();
+			stackHeight.pop();
+		}
+
+		return maxArea;
+    }
+
+	void test()
+	{
+		vector<int> height(1,0);
+		cout << largestRectangleArea(height) << endl;
+	}
+};
