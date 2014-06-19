74e42e1bfbe8f92ef16675a616a4ce17ea50df01
pysoftmac <pysoftmac@gmail.com>
2014-06-19 16:57:03 +0800

    need review

--- Largest Rectangle in Histogram.cpp
+++ Largest Rectangle in Histogram.cpp
@@ -2,49 +2,24 @@
 
 class Solution {
 public:
+	int Max(int a, int b) { return a > b ? a : b; }
     int largestRectangleArea(vector<int> &height) {
-		if((int)height.size() == 0)
-			return 0;
-
+		height.push_back(0);
+		stack<int> stk;
+		int i = 0;
 		int maxArea = 0;
-
-		stack<int> stackIndex;
-		stack<int> stackHeight;
-
-		for(int i = 0; i < (int)height.size(); i++)
+		while(i < (int)height.size())
 		{
-			if(stackIndex.empty() || height[i] > stackHeight.top())
+			if(stk.empty() || height[stk.top()] <= height[i])
 			{
-				stackIndex.push(i);
-				stackHeight.push(height[i]);
+				stk.push(i++);
 			}
 			else
 			{
-				if(height[i] < stackHeight.top())
-				{
-					int lastIndex = 0;
-					while(!stackIndex.empty() && height[i] <= stackHeight.top())
-					{
-						int tempArea = stackHeight.top() * (i - stackIndex.top());
-						if(tempArea > maxArea)
-							maxArea = tempArea;
-						lastIndex = stackIndex.top();
-						stackIndex.pop();
-						stackHeight.pop();
-					}
-					stackIndex.push(lastIndex);
-					stackHeight.push(height[i]);
-				}
+				int t = stk.top();
+				stk.pop();
+				maxArea = Max(maxArea, height[t] *(stk.empty() ? i : i - stk.top() - 1));
 			}
-		}
-
-		while(!stackIndex.empty())
-		{
-			int tempArea = stackHeight.top() * ((int)height.size() - stackIndex.top());
-			if(tempArea > maxArea)
-				maxArea = tempArea;
-			stackIndex.pop();
-			stackHeight.pop();
 		}
 
 		return maxArea;
